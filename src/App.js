5655f88f51db7ca6dede6408d9eae4d6ba5a424c
devansh6012 <devansh.g6012@gmail.com>
2022-03-02 15:46:20 +0530

    Favourite Page created, React Router Dom implemented, Stored in local storage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Banner from './Components/Banner';
 import Movies from './Components/Movies';
+import Favourite from './Components/Favourite';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar/>
-      <Banner/>
-      <Movies/>
-    </>
+      <Switch>
+      <Route path='/' exact render={(props)=>(
+          <>
+            <Banner {...props}/>
+            <Movies {...props}/>
+          </>
+        )}/>
+        <Route path='/favourites' component={Favourite} />
+      </Switch>
+    </Router>
   );
 }
 
--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -9,7 +9,8 @@ export default class Movies extends Component {
             hover:'',
             parr:[1],
             currPage:1,
-            movies:[]
+            movies:[],
+            favourites:[]
         }
     }
     async componentDidMount(){
@@ -56,6 +57,24 @@ export default class Movies extends Component {
             },this.changeMovies)
         }
     }
+    handleFavourites=(movie)=>{
+        let oldData = JSON.parse(localStorage.getItem("movies-app") || "[]")
+        if(this.state.favourites.includes(movie.id)){
+            oldData = oldData.filter((m)=>m.id!=movie.id)
+        }else{
+            oldData.push(movie)
+        }
+        localStorage.setItem("movies-app",JSON.stringify(oldData));
+        console.log(oldData);
+        this.handleFavouritesState();
+    }
+    handleFavouritesState=()=>{
+        let oldData = JSON.parse(localStorage.getItem("movies-app") || "[]")
+        let temp = oldData.map((movie)=>movie.id);
+        this.setState({
+            favourites:[...temp]
+        })
+    }
     render() {
         // let movie = movies.results
         // console.log('render');
@@ -72,14 +91,14 @@ export default class Movies extends Component {
                                 {
                                     this.state.movies.map((movieObj) => (
                                         <div className="card movies-card" onMouseEnter={()=>this.setState({hover:movieObj.id})} onMouseLeave={()=>this.setState({hover:''})}>
-                                            <img src={`https://image.tmdb.org/t/p/original${movieObj.backdrop_path}`} alt={this.state.movies.title} className="card-img-top banner-img" />
+                                            <img src={`https://image.tmdb.org/t/p/original${movieObj.backdrop_path}`} alt={this.state.movies.title} className="card-img-top movies-img" />
                                             {/* <div className="card-body"> */}
                                             <h5 className="card-title movies-title">{movieObj.original_title}</h5>
                                             {/* <p class="card-text movies-text">{movieObj.overview}</p> */}
                                             <div className='button-wrapper' style={{ display: 'flex', width: '100%', justifyContent: 'center' }}>
                                                 {
                                                     this.state.hover == movieObj.id && 
-                                                    <a className="btn btn-primary movies-button">Add to Favourite</a>
+                                                    <a className="btn btn-primary movies-button" onClick={()=>this.handleFavourites(movieObj)}>{this.state.favourites.includes(movieObj.id)?"Remove from favourites":"Add to favourites"}</a>
                                                 }
                                                 
                                             </div>
@@ -91,13 +110,13 @@ export default class Movies extends Component {
                             <div style={{display:'flex',justifyContent:'center'}}>
                             <nav aria-label="Page navigation example">
                                 <ul className="pagination">
-                                    <li className="page-item"><a className="page-link" onClick={this.handleLeft}>Previous</a></li>
+                                    <li className="page-item"><a className="page-link" onClick={this.handleLeft} style={{cursor:'pointer'}}>Previous</a></li>
                                     {
                                         this.state.parr.map((value)=>(
-                                            <li className="page-item"><a className="page-link" onClick={()=>this.handleClick(value)}>{value}</a></li>
+                                            <li className="page-item"><a className="page-link" onClick={()=>this.handleClick(value)} style={{cursor:'pointer'}}>{value}</a></li>
                                         ))
                                     }
-                                    <li className="page-item"><a className="page-link" onClick={this.handleRight}>Next</a></li>
+                                    <li className="page-item"><a className="page-link" onClick={this.handleRight} style={{cursor:'pointer'}}>Next</a></li>
                                 </ul>
                             </nav>
                             </div>
